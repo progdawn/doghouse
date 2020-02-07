6019c6215bcc4bc663b0b256dc832b69ba6ca6ed
Dawn Myers <dmyers@allbridge.com>
2020-02-07 13:37:27 -0600

    Butchering some CSS

--- index.php
+++ index.php
@@ -26,32 +26,24 @@
             </div>
         </nav>
 
-        <section>
-            <div class="container-fluid">
-                <?php
-                    $dirname = "dog-images/";
-                    $images = glob($dirname."*.jpg");
-                    $imageCounter = 0;
 
-                    foreach($images as $image) {
-                        if($imageCounter % 3 === 0) {
-                            echo '<div class="row justify-content-md-center">';
-                        }
-                        echo '<div class="d-flex flex-column"><img src="'.$image.'" class="img-fluid m-1 p-1"></div>';
-                        if($imageCounter % 3 === 2){
-                            echo '</div>';
-                        }
-                        $imageCounter++;
-                        if($image === end($images)){
-                            echo '</div>';
-                        }
-                    }
-                ?>
-            </div>
-        </section>
+        <div class="masonry">
+            <?php
+                $dirname = "dog-images/";
+                $images = glob($dirname."*.jpg");
+                $imageCounter = 0;
+
+                foreach($images as $image) {
+                    echo '<div class="brick"><img src="'.$image.'"></div>';
+                    $imageCounter++;
+                }
+            ?>
+        </div>
+
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="js/main.js"></script>
     </body>
 </html>
