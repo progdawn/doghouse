b625bfea71de436c60446d8ffbf69160f92c51e1
Dawn Myers <dmyers@allbridge.com>
2020-02-17 13:40:18 -0600

    Made images prettier

--- index.php
+++ index.php
@@ -35,19 +35,22 @@
             </div>
         </nav>
 
+        <section class="gallery-block grid-gallery">
+            <div class="container">
+                <div class="row">
+                    <?php
+                        $dirname = "dog-images/*";
+                        $images = glob($dirname);
+                        $imageCounter = 0;
 
-        <div class="masonry">
-            <?php
-                $dirname = "dog-images/*";
-                $images = glob($dirname);
-                $imageCounter = 0;
-
-                foreach($images as $image) {
-                    echo '<div class="brick"><img src="'.$image.'"></div>';
-                    $imageCounter++;
-                }
-            ?>
-        </div>
+                        foreach($images as $image) {
+                            echo '<div class="col-md-6 col-lg-4 item"><img class="img-fluid image scale-on-hover" src="'.$image.'"></div>';
+                            $imageCounter++;
+                        }
+                    ?>
+                </div>
+            </div>
+        </section>   
 
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
