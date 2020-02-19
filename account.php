d7afb398b73658014d86196893a1a2ccef1af70e
Dawn Myers <dmyers@allbridge.com>
2020-02-19 12:42:15 -0600

    Account displays user images

--- account.php
+++ account.php
@@ -43,13 +43,45 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             </div>
         </nav>
 
-        <div class="container">
-            <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-        </div>
-        <div class="container">
-            <p><a href="reset-password.php" class="btn btn-warning">Reset Your Password</a></p>
-            <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
-        </div>
+        <section>
+            <div class="container">
+                <h1>Welcome back, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
+            </div>
+            <div class="container">
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="reset-password.php" class="btn btn-link">Reset Password</a></li>
+                    <li class="list-group-item"><a href="logout.php" class="btn btn-link">Sign Out</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="gallery-block grid-gallery">
+            <div class="container">
+                <div class="row">
+                    <?php
+                        include ("connect.php");
+
+                        $sql = "SELECT name FROM images WHERE user = ?";
+
+                        if($stmt = mysqli_prepare($link, $sql)){
+                            mysqli_stmt_bind_param($stmt, "s", $param_user);
+                        
+                            // Set parameters
+                            $param_user = $_SESSION["username"]; 
+                            if(mysqli_stmt_execute($stmt)){
+                                $result = $stmt->get_result();
+                                while($row = $result->fetch_array(MYSQLI_NUM)){
+                                    foreach($row as $r)
+                                    echo '<div class="col-md-6 col-lg-4 item"><img class="img-fluid image scale-on-hover" src="'.$r.'"></div>';
+                                }
+                            }
+                            mysqli_stmt_close($stmt);
+                        }
+                        mysqli_close($link);
+                    ?>
+                </div>
+            </div>
+        </section>   
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
